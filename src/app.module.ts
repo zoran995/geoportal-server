d9ed8b7de69000684c4a773e5cae6353516291fe
Zoran Kokeza <zorankokeza95@gmail.com>
2021-07-12 03:00:52 +0200

    add correct mime types

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,8 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { CustomConfigService } from './config/config.service';
 import { ServeStaticDto } from './config/dto/serve-static.dto';
 import { existsSync } from 'fs';
+import { application } from 'express';
+import { extname } from 'path';
 
 @Module({
   imports: [
@@ -53,6 +55,14 @@ import { existsSync } from 'fs';
             serveStaticOptions: {
               dotfiles: 'ignore',
               index: serveStatic.resolvePathRelativeToWwwroot,
+              setHeaders: (res, path) => {
+                const type = extname(path);
+                if (type === '.czml' || type === '.geojson') {
+                  res.header('Content-type', 'application/json');
+                } else if (type === '.glsl') {
+                  res.header('Content-type', 'text/plain');
+                }
+              },
             },
           },
         ];
