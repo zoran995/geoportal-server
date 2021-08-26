5837a3d7aa4f7416a5298532b080bbcb471a70db
Zoran Kokeza <zorankokeza95@gmail.com>
2021-08-27 01:29:43 +0200

    combine urls tests

---
+++ src/common/helpers/combineURLs.spec.ts
@@ -0,0 +1,19 @@
+import { combineURLs } from './combineURLs';
+
+describe('combineUrls', () => {
+  const url = 'http://test.com/';
+
+  it('properly combines', () => {
+    const id = 'test';
+    expect(combineURLs(url, id)).toBe(`${url}${id}`);
+  });
+
+  it('properly joins with multiple slashes', () => {
+    const id = 'test';
+    expect(combineURLs(`${url}//`, `//${id}`)).toBe(`${url}${id}`);
+  });
+
+  it('returns base url', () => {
+    expect(combineURLs(url)).toBe(url);
+  });
+});
--- src/common/helpers/combineURLs.ts
+++ src/common/helpers/combineURLs.ts
@@ -5,7 +5,7 @@
  * @param relativeURL - The relative URL
  * @returns The combined URL
  */
-export function combineURLs(baseURL: string, relativeURL: string): string {
+export function combineURLs(baseURL: string, relativeURL?: string): string {
   return relativeURL
     ? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '')
     : baseURL;
