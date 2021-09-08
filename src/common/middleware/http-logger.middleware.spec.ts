9129c796e669f9d535b2b4af55a755a6d62d4dac
Zoran Kokeza <zorankokeza95@gmail.com>
2021-09-08 20:31:51 +0200

    test http logger middleware

--- src/common/middleware/http-logger.middleware.spec.ts
+++ src/common/middleware/http-logger.middleware.spec.ts
@@ -1,7 +1,39 @@
+import { Request } from 'express';
+import { LoggerService } from '../logger/logger.service';
 import { HttpLoggerMiddleware } from './http-logger.middleware';
 
+jest.mock('src/common/logger/logger.service');
+
+const reqMockObject: Partial<Request> = {
+  body: 'test-body',
+  params: { test: 'params' },
+  query: { test: 'query' },
+  originalUrl: '/test',
+  protocol: 'http',
+  get: jest.fn().mockReturnValue('example.com'),
+};
+
+const mockNext = jest.fn();
+const mockRes = {};
+
 describe('HttpLoggerMiddleware', () => {
+  let httpLogger: HttpLoggerMiddleware;
+  beforeEach(() => {
+    (LoggerService as any).mockClear();
+    httpLogger = new HttpLoggerMiddleware();
+  });
+
   it('should be defined', () => {
-    expect(new HttpLoggerMiddleware()).toBeDefined();
+    expect(httpLogger).toBeDefined();
+  });
+
+  it('properly logs info and calls next', () => {
+    httpLogger.use(reqMockObject as any, mockRes as any, mockNext);
+    expect(LoggerService).toHaveBeenCalled();
+    expect((LoggerService as any).mock.instances).toHaveLength(1);
+    const mockLoggerService = (LoggerService as any).mock.instances[0];
+
+    expect(mockLoggerService.verbose.mock.calls.length).toBeGreaterThan(1);
+    expect(mockNext).toHaveBeenCalledTimes(1);
   });
 });
