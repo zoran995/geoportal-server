97966b50fcd66411c2ff3b24fbe612f98edc9841
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-11-26 12:02:09 +0100

    test: add proj4 endpoint e2e tests

---
+++ test/proj4.e2e-spec.ts
@@ -0,0 +1,40 @@
+import type { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
+
+import { AppModule } from 'src/app.module';
+
+describe('Proj4 endpoint (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.setGlobalPrefix('api');
+    await app.init();
+  });
+
+  it('should return the correct proj4 definition', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/api/proj4def/epsg:4326')
+      .expect(200);
+
+    expect(response.type).toEqual('text/plain');
+    expect(response.text).toEqual('+proj=longlat +datum=WGS84 +no_defs');
+  });
+
+  it('should return 400 on string EPSG code', async () => {
+    await request(app.getHttpServer())
+      .get('/api/proj4def/epsg:test')
+      .expect(400);
+  });
+
+  it('should return 404 when definition is not found', async () => {
+    await request(app.getHttpServer())
+      .get('/api/proj4def/epsg:999999')
+      .expect(404);
+  });
+});
