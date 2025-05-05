ee467057f6ba8f6027d85b997b7f06bedaff3aa8
Zoran Kokeza <zorankokeza95@gmail.com>
2025-05-05 20:55:49 +0200

    add got http service

--- src/infrastructure/http/app-http.module.ts
+++ src/infrastructure/http/app-http.module.ts
@@ -1,10 +1,13 @@
 import { HttpModule as BaseHttpModule } from '@nestjs/axios';
 import { Global, Module } from '@nestjs/common';
 
+import got from 'got';
 import http from 'http';
 import https from 'https';
 
+import { AppHttpService } from './app-http-service.js';
 import { AxiosLogInterceptor } from './axios-log-interceptors.js';
+import { GOT_INSTANCE_TOKEN } from './constants.js';
 
 const agentConfig: https.AgentOptions = {
   keepAlive: true,
@@ -21,7 +24,14 @@ const agentConfig: https.AgentOptions = {
       httpsAgent: new https.Agent(agentConfig),
     }),
   ],
-  providers: [AxiosLogInterceptor],
-  exports: [BaseHttpModule],
+  providers: [
+    {
+      provide: GOT_INSTANCE_TOKEN,
+      useValue: got.extend({}),
+    },
+    AppHttpService,
+    AxiosLogInterceptor,
+  ],
+  exports: [BaseHttpModule, AppHttpService],
 })
-export class AppHttpModule extends BaseHttpModule {}
+export class AppHttpModule {}
